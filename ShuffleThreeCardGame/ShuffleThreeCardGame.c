b50ca1f42126ca279f4cbd5d0886dda3179244ee
Orel Mordechai <44357944+ORELxD@users.noreply.github.com>
2019-04-07 19:50:10 +0300

    C

---
+++ ShuffleThreeCardGame/ShuffleThreeCardGame.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/*
+ * There are three cards - Jack,king and queen.
+ * Computer shuffle these three cards randomly
+ * and you have to guess the position of the king.
+*/
+
+int cash = 100;
+
+void playgame(int betting_amount)
+{
+	char A[3] = { 'J','K','Q' };
+	int playerschoice, i;
+	srand(time(NULL));
+	for (i = 1; i <= 10; i++)
+	{
+		int x = rand() % 3;
+		int y = rand() % 3;
+		int temp = A[x];
+		A[x] = A[y];
+		A[y] = temp;
+	}
+
+	printf("Shuffling is done\n");
+	printf("What is the position of king(1,2 or 3)? ");
+	scanf("%d", &playerschoice);
+	if (A[playerschoice] == 'K')
+	{
+		cash = cash + 2 * betting_amount;
+		printf("\nYou win !!!      Position of cards= '%c' '%c' '%c'    your current balance= $%d\n", A[0], A[1], A[2], cash);
+	}
+	else
+	{
+		cash = cash - betting_amount;
+		printf("\nYou lose !!!     Position of cards= '%c' '%c' '%c'    your current balance= $%d\n", A[0], A[1], A[2], cash);
+	}
+
+}
+
+
+int main()
+{
+	int betting_amount;
+	printf("~$~$~$~$~$~$ Welcome to my casino $~$~$~$~$~$~");
+	printf("\n\nSimple Rules:\nThere are three cards - Jack,king and queen.\nComputer shuffle these three cards randomly and you have to guess the position of the king.\n");
+	printf("\n\nTotal cash = %d$", cash);
+	while (cash > 0)
+	{
+		printf("\nWhat is your betting amount?$");
+		scanf("%d", &betting_amount);
+		if (betting_amount == 0 || betting_amount > cash)
+			break;
+		playgame(betting_amount);
+	}
+	printf("Done !!! No more money :(\n\n\n");
+}
